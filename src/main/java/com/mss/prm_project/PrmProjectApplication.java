2dbb2696dc9d23e19d4774c5ce0ef8f73cef391b
Tung Pham <tungpvhe186583@fpt.edu.vn>
2025-10-02 15:45:15 +0700

    feat: First commnit

---
+++ src/main/java/com/mss/prm_project/PrmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mss.prm_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrmProjectApplication.class, args);
+    }
+
+}

b1ff0b67bd9aef295506b6413109e59e23e32807
BuiMinhChien <chien181004@gmail.com>
2025-11-06 16:04:26 +0700

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO roles (role_name, delete_flag) VALUES (N'STUDENT', FALSE);
 -- Users - Password là "123"
 INSERT INTO users (
     email, username, password, phone_number, full_name, avatar_url,
-    role_id, enabled, push_notifications, email_notifications,
+    role_id, enabled, instant_push_notification, scheduled_push_notification,
     fcm_token, delete_flag
 )
 VALUES
@@ -39,7 +39,6 @@ VALUES
      2, TRUE, TRUE, TRUE,
      'ctGH7JHMQw2l3qYL-Mw6SI:APA91bFdp0pL-2giW8n_1jvSmWAMbkTvskTLsaXnjbH8z7ctyco3J0osSRlYoR7cGIs07Yi4zLiazdlLOAUoDBNWPLUzlJGbL4pZczBeRY1pfd3pHNIgcGY',
      FALSE),
-
     ('rs3@gmail.com', 'researcher_03', '$2a$12$ShPV6hcFmFJGiJ0H9f78CeIyNHmz42MclkvgN60VVhaCV.OlcOKba',
      '0903000003', N'Hoàng Đức Tài', 'https://i.pinimg.com/originals/7b/1e/b9/7b1eb944285fe8822ebe3fc0a036e1f9.png',
      2, TRUE, TRUE, TRUE,
